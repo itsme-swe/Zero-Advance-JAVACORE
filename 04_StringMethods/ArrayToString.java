983d5b75f8ab962d59017225b465650721e6b9c7
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-05 18:55:02 +0530

    update

---
+++ 04_StringMethods/ArrayToString.java
@@ -0,0 +1,14 @@
+/**
+ * Here we are converting char array into string
+ */
+public class ArrayToString {
+    public static void main(String[] args) {
+        char [] c = {'H','A','R','S','H'};
+
+        String name = new String(c);
+
+        System.out.println(name);
+    }
+
+    
+}
---
+++ 04_StringMethods/ToLowerToUpper.java
@@ -0,0 +1,20 @@
+/**
+ * String methods toLower() and toUpper() to convert cases.
+ */
+public class ToLowerToUpper {
+    public static void main(String[] args) {
+        String carName = "virtus";
+
+        String str = carName.toUpperCase();
+
+        System.out.println(str);
+
+        String company = "VOLKSWAGEN";
+
+        String newStr = company.toLowerCase();
+
+        System.out.println(newStr);
+    }
+
+    
+}
