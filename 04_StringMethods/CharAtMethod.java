36048c10185101ae63c9071ae07794fd2f04fc98
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-06 11:04:39 +0530

    update

---
+++ 04_StringMethods/CharAtMethod.java
@@ -0,0 +1,10 @@
+public class CharAtMethod {
+    public static void main(String[] args) {
+        
+        String car = "Maruti Jimmny";
+
+        char c1 = car.charAt(5);
+
+        System.out.println(c1);     // output: i
+    }
+}
---
+++ 04_StringMethods/StartsEndsWithMethod.java
@@ -0,0 +1,12 @@
+/**
+ * startsWith() and endsWith() Method returns boolean value and used to check the begining and end of the string
+ */
+public class StartsEndsWithMethod {
+
+    public static void main(String[] args) {
+        String s1 = "WWW.harh@gmail.com";
+
+        System.out.println(s1.startsWith("WWW"));        // Output: true
+        System.out.println(s1.endsWith("com"));       // Output: true
+    }
+}
