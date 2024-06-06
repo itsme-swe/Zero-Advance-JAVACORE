cb12217c5d12e09d512f9a1aa04c5d7f5b2d8933
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-06 13:24:35 +0530

    update

---
+++ 04_StringMethods/ConcatMethod.java
@@ -0,0 +1,13 @@
+/**
+ * Concat Method is used to join the two strings. Instead of usig concat() method we can use "+" operator to joi the two strings
+ */
+public class ConcatMethod {
+
+    public static void main(String[] args) {
+        
+        String str1 = "Welcome to";
+        String str2 = " the jungle";
+
+        System.out.println(str1.concat(str2));
+    }
+}
---
+++ 04_StringMethods/ContainsMethods.java
@@ -0,0 +1,12 @@
+/*
+   Contains Methods used to check wheter the element present in string or ot. And it return boolean value
+ */
+public class ContainsMethods {
+
+    public static void main(String[] args) {
+        
+        String car = "I own Virtus GT Edition car";
+
+        System.out.println(car.contains("GT"));     // Output: true
+    }
+}
