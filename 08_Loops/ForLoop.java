829a43d415d41761fe9003d12919e3cd65fd5f19
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-07 16:27:19 +0530

    update

--- 08_Loops/ForLoop.java
+++ 08_Loops/ForLoop.java
@@ -10,4 +10,11 @@ public class ForLoop {
     }
 }
 
-// Output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
+/* 
+🔸Output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+◽Syntax of forLoop  -- for (initilisation, condition, updation){
+                                body_ _ __ _ __ _ _
+                                _ _ _ _ _ _ _ _ _ _ _ _        
+                        }
+*/  
---
+++ 08_Loops/PrintTable.java
@@ -0,0 +1,28 @@
+/**
+ * Printing table using for loop
+ */
+public class PrintTable {
+
+    public static void main(String[] args) {
+        
+        int n = 5;
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println("5" + " * " + i +" = " + n * i);
+        }
+    }
+}
+
+/*
+    ◽Output:
+        5 * 1 = 5
+        5 * 2 = 10
+        5 * 3 = 15
+        5 * 4 = 20
+        5 * 5 = 25
+        5 * 6 = 30
+        5 * 7 = 35
+        5 * 8 = 40
+        5 * 9 = 45
+        5 * 10 = 50
+ */
