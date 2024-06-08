81df88bc7200c3189505d236b4214b424ea9d6c3
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-08 16:41:26 +0530

    update

---
+++ 09_StringBufferClass/Insert_Method.java
@@ -0,0 +1,12 @@
+/**
+ * Insert_Method used to insert the element at particular index value.
+ */
+public class Insert_Method {
+
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("Skoda Savia");
+
+        System.out.println(sb.insert(7, "l"));      //🔸Output: Skoda Slavia
+    }
+}
---
+++ 09_StringBufferClass/Reverse_Method.java
@@ -0,0 +1,12 @@
+/**
+ * Reverse_Method used to reverse the characters within the string buffer
+ */
+public class Reverse_Method {
+
+    public static void main(String[] args) {
+        
+        StringBuffer sb = new StringBuffer("Hello World");
+
+        System.out.println(sb.reverse());       //🔸Output: dlroW olleH
+    }
+}
