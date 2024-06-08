18326a8189cb8cee30c628555a0b667c9dc1ceaf
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-08 08:15:25 +0530

    update

---
+++ 09_CompleteString/Constructors.java
@@ -0,0 +1,14 @@
+
+/**
+ * Constructors in String Class
+ */
+public class Constructors {
+
+    public static void main(String[] args) {
+        byte[] b = {101, 102, 103};
+
+        String s1 = new String(b);
+
+        System.out.print(s1 + " ");     // Output: efg
+    }
+}
