207d3efffa68ff2a4462b6a2bbdec217149d958b
Harsh Mehra <me.harshmehra07@gmail.com>
2024-06-10 19:53:44 +0530

    update

---
+++ 14_ObjectOrientedProgramming/Constructor_Method.java
@@ -0,0 +1,33 @@
+/**
+ * Constructor_Method
+ */
+public class Constructor_Method {
+
+    public static void main(String[] args) {
+        
+        Rectangle r = new Rectangle(10,5);
+
+        
+
+
+    }
+}
+
+class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    //🔸Constructors
+    public Rectangle()
+    {
+        length = 1;
+        breadth = 1;
+    }
+
+    public Rectangle(int l, int b)
+    {
+        length = l;
+        breadth = b;
+    }
+}
